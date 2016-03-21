9911cfb28df17c2a7b2d013ecf2e23b0903b63f4
Will Worth <will@willworth.co.uk>
2016-03-21 09:38:08 +0100

    first
    first

---
+++ src/uk/co/willworth/calcengine/Main.java
@@ -0,0 +1,38 @@
+package uk.co.willworth.calcengine;
+
+public class Main {
+
+    public static void main(String[] args) {
+    double[] leftVals = {100.0d, 25.0d, 225.0d, 11.0d};
+    double[] rightVals = { 50.0d, 92.0d, 17.0d, 3.0d};
+    char[] opCodes = {'d', 'a', 's', 'm'};
+    double[] results new double[opCodes.length];
+
+
+//	double val1 = 100.0d;
+//    double val2 =50.0d;
+//    double result;
+//    char opCode='d';
+
+    if (opCode == 'a')
+        result = val1 + val2;
+
+    else if(opCode == 's')
+        result = val1 - val2;
+
+    else if(opCode == 'd')
+        result = val2!=0.0d ? val1 / val2 :0.0d;
+//            if (val2 !=0.0d)
+//            result= val1/val2;
+//        else result = 0.0d;
+    else if(opCode== 'm')
+        result= val1 * val2;
+    else {
+        System.out.println("error:invalid opcode");
+        result = 0.0d;
+    }
+
+    System.out.println(result);
+
+    }
+}
