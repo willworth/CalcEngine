71ab5b871dcc12c11faf0b13ee735d88ed41478f
Will Worth <will@willworth.co.uk>
2016-05-05 17:46:52 +0200

    experimenting with github app
    main class first commit_

--- src/uk/co/willworth/calcengine/Main.java
+++ src/uk/co/willworth/calcengine/Main.java
@@ -6,33 +6,39 @@ public class Main {
     double[] leftVals = {100.0d, 25.0d, 225.0d, 11.0d};
     double[] rightVals = { 50.0d, 92.0d, 17.0d, 3.0d};
     char[] opCodes = {'d', 'a', 's', 'm'};
-    double[] results new double[opCodes.length];
+    double[] results = new double[opCodes.length];
 
 
 //	double val1 = 100.0d;
 //    double val2 =50.0d;
 //    double result;
 //    char opCode='d';
+    for (int i = 0; i< opCodes.length; i++) {
+        switch (opCodes[i]) {
+            case 'a':
+                results[i] = leftVals[i] + rightVals[i];
+                break;
+            case 's':
+                results[i] = leftVals[i] - rightVals[i];
+                break;
+            case 'd':
+                results[i] = rightVals[i] != 0.0 ? leftVals[i] / rightVals[i] : 0.0;
+                break;
+            case 'm':
+                results[i] = leftVals[i] * rightVals[i];
+                break;
+            default:
+                System.out.println("Error - invalid opCode");
+                results[i] = 0.0d;
+                break;
 
-    if (opCode == 'a')
-        result = val1 + val2;
+        }
 
-    else if(opCode == 's')
-        result = val1 - val2;
-
-    else if(opCode == 'd')
-        result = val2!=0.0d ? val1 / val2 :0.0d;
-//            if (val2 !=0.0d)
-//            result= val1/val2;
-//        else result = 0.0d;
-    else if(opCode== 'm')
-        result= val1 * val2;
-    else {
-        System.out.println("error:invalid opcode");
-        result = 0.0d;
     }
-
-    System.out.println(result);
+        for(double theResult: results) {
+            System.out.print("The result =");
+            System.out.println(theResult);
+        }
 
     }
 }
